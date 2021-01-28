c849e4d4d40f90b6383dedcb47dc3f8ccb5e45a9
Annu10 <annusoni001@gmail.com>
2021-01-28 23:15:44 +0530

    code cleaning

--- booking_app/views.py
+++ booking_app/views.py
@@ -53,10 +53,6 @@ def login_view(request, *args, ** kwargs):
     form = UserLoginForm(request.POST or None)
     if request.method == "POST":
         if form.is_valid():
-            print("was here")
-            #user = form.save(commit = False)
-            #username = form.cleaned_data.get('username')
-            #password = form.cleaned_data.get('password')
             username = request.POST.get('username')
             password = request.POST.get('password')
             user = authenticate(username =username, password = password)
@@ -66,9 +62,6 @@ def login_view(request, *args, ** kwargs):
                     redirect(next)
                 return redirect('/home')
         else:
-            print("gotchaaaa")
-            #messages.warning(request, "login falied")
-            #args['form'] = form #instead of PasswdForm()
             context = {
             'form' : form
             }    
@@ -130,11 +123,7 @@ def select_floor_shift(request, *args, ** kwargs):
             booking_date = request.POST.get('booking_date')
             floor = request.POST.get('floor')
             shift = request.POST.get('shift')
-            print("booking date was "+ booking_date)
-            print("floor was "+floor)
-            print("shift was "+ shift)
 
-            #TODO move seat fetching to a dao function
             all_seats = Seat.objects.filter(floor =floor)
             all_seats_a_1 = []
             all_seats_a_2 = []
@@ -155,8 +144,6 @@ def select_floor_shift(request, *args, ** kwargs):
             all_floors = Floor.objects.all()
             
             r = rev_date(booking_date)
-            print("r was")
-            print(r)
             all_bookings_of_date = SeatBooking.objects.filter(booking_date = r,shift = shift).order_by('seat_num')
             self_booked_ids = []
             booked_seat_ids = []
@@ -164,12 +151,6 @@ def select_floor_shift(request, *args, ** kwargs):
                 booked_seat_ids.append(b.seat_id)
                 if b.booked_by == request.user.username:
                     self_booked_ids.append(b.seat_id)
-
-
-            print(" filtered seats were")
-            print(booked_seat_ids)
-            if 1 in booked_seat_ids:
-                print("okay")
             context = {
             'all_seats' :  all_seats,
             'all_floors' : all_floors,
@@ -187,8 +168,6 @@ def select_floor_shift(request, *args, ** kwargs):
             'all_seats_c_2' : all_seats_c_2,
             'is_admin' : is_admin
             }
-            print("we did got here")
-            #messages.error(request, 'Floor added successfully!')
             return render(request, 'view_seats.html',context)
         else:
             print("gotchaaaa")
@@ -204,11 +183,9 @@ def select_floor_shift(request, *args, ** kwargs):
     }
     return render(request, "select_floor_n_shift.html", context)
 
-#BookSeatForm
 @login_required(login_url='login')
 def book_seat(request, *args, ** kwargs):
     is_admin = is_user_admin(request.user.email)
-    print("got here1")
     next = request.GET.get('next')
     form = BookSeatForm(request.POST or None)
     if request.method == "POST":
@@ -236,7 +213,6 @@ def book_seat(request, *args, ** kwargs):
             all_seats_c_2 = get_second_half_seats(floor, 'C')                
             all_floors = Floor.objects.all()
             r = rev_date(booking_date)
-            print("in book seat valid form")
 
             all_bookings_of_date = SeatBooking.objects.filter(booking_date = r,shift = shift)
             booked_seat_ids = []
@@ -245,9 +221,6 @@ def book_seat(request, *args, ** kwargs):
                 booked_seat_ids.append(b.seat_id)
                 if b.booked_by == request.user.username:
                     prev_self_booked_ids.append(b.seat_id)
-            print("booked seat ids are ")
-            print(booked_seat_ids)
-            print("seat_id got was"+seat_id)
             booking_possible = False
             if  int(seat_id) in prev_self_booked_ids:
                 messages.error(request, "You have already boooked this seat!!!!")
@@ -296,11 +269,8 @@ def book_seat(request, *args, ** kwargs):
             'all_seats_c_2' : all_seats_c_2,
             'is_admin' : is_admin
             }
-            print("we did got here")
-            #messages.error(request, 'Floor added successfully!')
             return render(request, 'view_seats.html',context)
         else:
-            print("gotchaaaa------")
             context = {
             'form' : form,
             'is_admin' : is_admin
@@ -316,15 +286,11 @@ def book_seat(request, *args, ** kwargs):
 @login_required(login_url='login')
 def cancel_booking(request, *args, ** kwargs):
     is_admin = is_user_admin(request.user.email)
-    print("got in cancel booking")
     next = request.GET.get('next')
     form = CancelBookingForm(request.POST or None)
     if request.method == "POST":
         if form.is_valid():
             booking_id = request.POST.get('id')
-            print("booking id got was "+booking_id)
-            #booking = SeatBooking.objects.filter(id =int(booking_id))
-            #print(booking)
             b = SeatBooking.objects.get(id=int(booking_id))  
             print(b)
             msg = ""
@@ -345,8 +311,6 @@ def cancel_booking(request, *args, ** kwargs):
                 'bookings' : bookings_for_user,
                 'is_admin' : is_admin
             }
-            print("we did got here")
-            #messages.error(request, 'Floor added successfully!')
             return render(request, 'cancel_booking_page.html',context)
         else:
             print("cancel booking form invalid")
@@ -362,6 +326,7 @@ def cancel_booking(request, *args, ** kwargs):
     }
     return render(request, "cancel_booking_page.html", context)
 
+@login_required(login_url='login')
 def cancel_booking_page(request, *args, ** kwargs):
     is_admin = is_user_admin(request.user.email)
     bookings_for_user = get_seat_bookings_for_user(request.user.username)
@@ -385,76 +350,81 @@ def cancel_booking_page(request, *args, ** kwargs):
 
     return render(request, 'cancel_booking_page.html', context)    
 
-
+@login_required(login_url='login')
 def add_floor(request, *args, ** kwargs):
     is_admin = is_user_admin(request.user.email)
-    print("testing config ="+settings.TEST_CONFIG)
-    print("got here1")
     next = request.GET.get('next')
     form = AddFloorForm(request.POST or None)
-    if request.method == "POST":
-        if form.is_valid():
-            print("was here in floor")
-            form.save()
-            #user = form.save(commit = False)
-            #username = form.cleaned_data.get('username')
-            #password = form.cleaned_data.get('password')
-            context = {
-            'form' : form,
-            'msg' : "Floor added Sucessfully",
-            'is_admin' : is_admin
-            }
-            messages.error(request, 'Floor added successfully!')
-            render(request, 'add_floor.html', context)
-        else:
-            floor_num = request.POST.get('floor_num')
-            print("  "+floor_num)
-            print("gotchaaaa")
-            #messages.warning(request, "login falied")
-            #args['form'] = form #instead of PasswdForm()
-            context = {
-            'form' : form,
-            'is_admin' : is_admin
-            }    
-            return render(request, 'add_floor.html', context)
+    context = {}
+    if is_admin == True:
+        if request.method == "POST":
+            if form.is_valid():
+                form.save()
+                context = {
+                'form' : form,
+                'msg' : "Floor added Sucessfully",
+                'is_admin' : is_admin
+                }
+                messages.error(request, 'Floor added successfully!')
+                render(request, 'add_floor.html', context)
+            else:
+                floor_num = request.POST.get('floor_num')
+                context = {
+                'form' : form,
+                'is_admin' : is_admin
+                }    
+                return render(request, 'add_floor.html', context)
 
-    context = {
-        'form' : form,
-        'is_admin' : is_admin
-    }
-    return render(request, "add_floor.html", context)
+        context = {
+            'form' : form,
+            'is_admin' : is_admin
+        }
+        return render(request, "add_floor.html", context)
+    else:
+        messages.error(request, 'Requested Page only available to admin users!')
+        context = {
+            'form' : form,
+            'is_admin' : is_admin
+        }
+        return render(request, "home.html", context)
 
+@login_required(login_url='login')
 def add_seat(request, *args, ** kwargs):
     is_admin = is_user_admin(request.user.email)
-    print("got here1")
     next = request.GET.get('next')
     form = AddSeatForm(request.POST or None)
-    if request.method == "POST":
-        if form.is_valid():
-            print("was here in seat")
-            form.save()
-            #user = form.save(commit = False)
-            #username = form.cleaned_data.get('username')
-            #password = form.cleaned_data.get('password')
-            context = {
-            'form' : form,
-            'is_admin' : is_admin
-            }
-            messages.error(request, 'Seat added successfully!')
-            return render(request, 'add_floor.html', context)
-        else:
-            print("gotchaaaa")
-            context = {
-            'form' : form,
-            'is_admin' : is_admin
-            }    
-            return render(request, 'add_floor.html', context)
+    context = {}
+    if is_admin == True:
+        if request.method == "POST":
+            if form.is_valid():
+                form.save()
+                context = {
+                'form' : form,
+                'is_admin' : is_admin
+                }
+                messages.error(request, 'Seat added successfully!')
+                return render(request, 'add_floor.html', context)
+            else:
+                print("gotchaaaa")
+                context = {
+                'form' : form,
+                'is_admin' : is_admin
+                }    
+                return render(request, 'add_floor.html', context)
 
-    context = {
-        'form' : form,
-        'is_admin' : is_admin
-    }
-    return render(request, "add_floor.html", context)
+        context = {
+            'form' : form,
+            'is_admin' : is_admin
+        }
+        return render(request, "add_floor.html", context)
+    else:
+        messages.error(request, 'Requested Page only available to admin users!')
+        context = {
+            'form' : form,
+            'is_admin' : is_admin
+        }
+        return render(request, "home.html", context)
+
 
 
 @login_required(login_url='login')
@@ -468,7 +438,6 @@ def logout_view(request):
     logout(request)
     return render(request, "index.html")
 
-#below are older tries
 def registerPage(request):
     if request.user.is_authenticated:
         return redirect('home')
